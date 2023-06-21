476358c460db84598f9d6fbbfeb6e9620c330cba
Carl John Ignacio Nicor <cjiNicor@gmail.com>
2023-06-21 15:17:52 +0800

    feat: added a moving platform mechanic

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    public float speed;
+    public int startPoint;
+    public Transform[] points;
+
+    private int i;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(transform);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.collider.transform.SetParent(null);
+    }
+
+}
