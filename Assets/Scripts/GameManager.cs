d50d0f5fae3a6cee7a91b621a9a14fb67079b7d5
Carl John Ignacio Nicor <cjiNicor@gmail.com>
2023-06-29 20:07:09 +0800

    refactor: refactored the score system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
     public Vector2 lastCheckpointPos;
     private Respawn _respawn;
+    private static int _scorePoints = 0;
+    private TMP_Text _scoreText;
 
     void Start()
     {
         _respawn = GameObject.FindGameObjectWithTag("Player").GetComponent<Respawn>();
         _respawn.transform.position = lastCheckpointPos;
+        _scoreText = GameObject.Find("Score").GetComponent<TMP_Text>();
+        _scoreText.text = "Score: " + _scorePoints;
     }
 
     void Awake()
@@ -22,5 +27,13 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    public void AddScore(int points)
+    {
+        _scorePoints += points;
+        _scoreText.text = "Score: " + _scorePoints;
+        Destroy(GameObject.FindGameObjectWithTag("Score"));
+        Debug.Log("Score: " + _scorePoints);
     }
 }
--- Assets/Scripts/ScoreUpdate.cs
+++ Assets/Scripts/ScoreUpdate.cs
@@ -1,26 +1,17 @@
 using UnityEngine;
-using TMPro;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class ScoreUpdate : MonoBehaviour
 {
-    private static int _scorePoints = 0;
-    private TMP_Text _scoreText;
+
+    private GameManager _gameManager;
 
     void Start()
     {
-        _scoreText = GameObject.Find("Score").GetComponent<TMP_Text>();
-        _scoreText.text = "Score: " + _scorePoints;
+        _gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            _scorePoints += 100;
-            _scoreText.text = "Score: " + _scorePoints;
-            Debug.Log("Score: " + _scorePoints);
-            Destroy(gameObject);
-        }
+        _gameManager.AddScore(100);
     }
 }
